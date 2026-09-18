641dcd48ce841b8dfed0ac498f5daf6762708ba3
errormoralesdavid-sudo <errormoralesdavid@gmail.com>
2026-09-18 11:20:39 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-self.addEventListener('fetch', function(event) {
-  // Service worker vacio para cumplir con el requisito de instalacion PWA
+const CACHE_NAME = 'yakuza-fade-v1-cache';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
 });
